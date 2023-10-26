0af1047e32ef1afa5e1e6b8d1cc3a67073722882
Slavouz <tmbwpva@protonmail.com>
2023-10-26 09:46:18 +0700

    Modul 3 On Progress

---
+++ Modul 3/Latihan/kegiatan1.py
@@ -0,0 +1,10 @@
+data = ["3 minggu 3 hari 7 jam 21 menit",
+        "5 minggu 5 hari 8 jam 11 menit",
+        "7 minggu 1 hari 5 jam 33 menit"]
+
+item_data = []
+
+for item in data:
+    item_data.append(item)
+
+print(item_data[0])
